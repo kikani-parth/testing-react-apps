c72b0e62d5905c98d7c486548f22af79f2a04afc
Parth Kikani <parthkikani02@gmail.com>
2024-05-30 11:02:09 +0300

    complete 05.js exercise and extra credit 1, 2

---
+++ src/__tests__/exercise/05-extra-2.js
@@ -0,0 +1,60 @@
+// mocking HTTP requests
+// http://localhost:3000/login-submission
+
+import * as React from "react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { build, fake } from "@jackfranklin/test-data-bot";
+import { setupServer } from "msw/node";
+import Login from "../../components/login-submission";
+import { handlers } from "test/server-handlers.js";
+
+const buildLoginForm = build({
+  fields: {
+    username: fake((f) => f.internet.userName()),
+    password: fake((f) => f.internet.password()),
+  },
+});
+
+const server = setupServer(...handlers);
+
+// before all the tests, start the server with `server.listen()`
+beforeAll(() => server.listen());
+
+//afterEach(() => server.resetHandlers())
+
+// after all the tests, stop the server with `server.close()`
+afterAll(() => server.close());
+
+test(`logging in displays the user's username`, async () => {
+  render(<Login />);
+  const { username, password } = buildLoginForm();
+
+  await userEvent.type(screen.getByLabelText(/username/i), username);
+  await userEvent.type(screen.getByLabelText(/password/i), password);
+
+  await userEvent.click(screen.getByRole("button", { name: /submit/i }));
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i));
+
+  expect(screen.getByText(username)).toBeInTheDocument();
+});
+
+test(`omitting the password results in an error`, async () => {
+  render(<Login />);
+
+  // Not going to fill in the password
+  const { username } = buildLoginForm();
+
+  await userEvent.type(screen.getByLabelText(/username/i), username);
+
+  await userEvent.click(screen.getByRole("button", { name: /submit/i }));
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i));
+
+  expect(screen.getByRole("alert")).toHaveTextContent("password required");
+});

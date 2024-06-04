dacbaa03b9f7c1c7e02e346a637d281e5cdab4d9
Parth Kikani <parthkikani02@gmail.com>
2024-06-04 11:15:07 +0300

    complete 08.js extra credit 2, 3

--- src/__tests__/exercise/08-extra-2.js
+++ src/__tests__/exercise/08-extra-2.js
@@ -1,11 +1,22 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
 
-import { act, renderHook } from "@testing-library/react";
+import * as React from "react";
+import { render, act } from "@testing-library/react";
 import useCounter from "../../components/use-counter";
 
+function setup({ initialProps } = {}) {
+  const result = {};
+  function TestComponent() {
+    result.current = useCounter(initialProps);
+    return null;
+  }
+  render(<TestComponent />);
+  return result;
+}
+
 test("exposes the count and increment/decrement functions", () => {
-  const { result } = renderHook(useCounter);
+  const result = setup();
 
   expect(result.current.count).toBe(0);
 
@@ -17,14 +28,12 @@ test("exposes the count and increment/decrement functions", () => {
 });
 
 test("allows customization of the initial count", () => {
-  const { result } = renderHook(useCounter, {
-    initialProps: { initialCount: 3 },
-  });
+  const result = setup({ initialProps: { initialCount: 3 } });
   expect(result.current.count).toBe(3);
 });
 
 test("allows customization of the step", () => {
-  const { result } = renderHook(useCounter, { initialProps: { step: 2 } });
+  const result = setup({ initialProps: { step: 2 } });
 
   expect(result.current.count).toBe(0);
 
@@ -33,20 +42,4 @@ test("allows customization of the step", () => {
 
   act(() => result.current.decrement());
   expect(result.current.count).toBe(0);
-});
-
-test(`the step can be changed`, () => {
-  const { result, rerender } = renderHook(useCounter, {
-    initialProps: { step: 2 },
-  });
-  expect(result.current.count).toBe(0);
-
-  act(() => result.current.increment());
-  expect(result.current.count).toBe(2);
-
-  // changing the step
-  rerender({ step: 1 });
-
-  act(() => result.current.decrement());
-  expect(result.current.count).toBe(1);
 });
